293a9f06f678a0f2ca16f116e65908c8574ef4f2
administrator <2316696377@qq.com>
2020-09-17 14:55:24 +0800

    init

---
+++ src/main/java/com/fy/dockerdemo/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.fy.dockerdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("")
+    public String message(){
+        return "hello, docker";
+    }
+}
